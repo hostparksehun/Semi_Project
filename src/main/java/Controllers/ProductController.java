9aa870a66e3798c30491a575398c96c7f12f394c
PSH <roten123_@naver.com>
2022-05-19 00:43:22 +0900

    src/main/java/Controllers/ProductController.java modified

--- src/main/java/Controllers/ProductController.java
+++ src/main/java/Controllers/ProductController.java
@@ -1,19 +1,38 @@
 package Controllers;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DAO.ProductDAO;
+import DTO.ProductDTO;
+
 @WebServlet("/ProductController")
 public class ProductController extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String uri = request.getRequestURI();
+		ProductDAO dao = new ProductDAO();
+
+		try {	
+			if(uri.equals("/info.ProductController")) {
+
+
+				List<ProductDTO> list;
+				list = dao.selectAll();
+			}   
+		}catch(Exception e) {
+			e.printStackTrace();
+			response.sendRedirect("error.html");
+		}
+
 	}
-
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}

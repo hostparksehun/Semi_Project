3a351653badabe8a3b23aae2bd1ae20994d500c3
PSH <roten123_@naver.com>
2022-05-18 23:20:29 +0900

    src/main/java/Controllers/ProductInfoController.java created

---
+++ src/main/java/dao/ListDAO.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class ListDAO {
+
+	private static ListDAO instance = null;
+	
+	public synchronized static ListDAO getlnstance() {
+		
+		if(instance == null) {
+			instance = new ListDAO();
+		}
+		return instance;
+		
+	}
+	
+	private Connection getConnection() throws Exception{
+		Context ctx = new InitialContext();
+		DataSource ds =(DataSource)ctx.lookup("java:comp/env/jdbc/oracle");
+		return ds.getConnection();
+	}
+	
+	private Connection getConnection() throws Exception {
+		
+		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","semi","semi");
+		
+	}
+	
+	public List<ListDTO> selectAll() throws Exception{
+		String sql = "select * form product_info"
+	}
+	
+	
+	
+	
+	
+}

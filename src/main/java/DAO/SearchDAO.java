deff40e0197eda0e3b6446ac4e80a253e0d9576f
PSH <roten123_@naver.com>
2022-05-25 09:49:43 +0900

    src/main/java/DAO/SearchDAO.java modified

--- src/main/java/DAO/SearchDAO.java
+++ src/main/java/DAO/SearchDAO.java
@@ -26,7 +26,7 @@ public class SearchDAO {
 
 	private Connection getConnection() throws Exception{
 		Context ctx = new InitialContext();
-		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/orcl");
+		DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/oracle");
 
 		return ds.getConnection();
 	}
